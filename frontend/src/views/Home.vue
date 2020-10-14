b67cd27c6a6ce8b1468aa56f07fa1aa44af85258
danieltan2018 <50669678+danieltan2018@users.noreply.github.com>
2020-10-15 00:55:08 +0800

    Revert about section tag

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -59,23 +59,25 @@
         </v-theme-provider>
       </v-img>
 
-      <div class="py-12"></div>
-      <v-container class="text-center">
-        <h2 class="display-2 font-weight-bold mb-3">What is commonette?</h2>
-        <v-responsive class="mx-auto mb-8" width="56">
-          <v-divider class="mb-1"></v-divider>
-          <v-divider></v-divider>
-        </v-responsive>
-        <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720">
-          info about commonette here
-        </v-responsive>
-        <v-btn color="grey" v-on:click="navigateRoute('/recommend')" outlined large>
-          <span class="grey--text text--darken-1 font-weight-bold">
-            Recommendation Page (temporary)
-          </span>
-        </v-btn>
-      </v-container>
-      <div class="py-12"></div>
+      <section id="about">
+        <div class="py-12"></div>
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3">What is commonette?</h2>
+          <v-responsive class="mx-auto mb-8" width="56">
+            <v-divider class="mb-1"></v-divider>
+            <v-divider></v-divider>
+          </v-responsive>
+          <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720">
+            info about commonette here
+          </v-responsive>
+          <v-btn color="grey" v-on:click="navigateRoute('/recommend')" outlined large>
+            <span class="grey--text text--darken-1 font-weight-bold">
+              Recommendation Page (temporary)
+            </span>
+          </v-btn>
+        </v-container>
+        <div class="py-12"></div>
+      </section>
 
       <section id="mediums" class="grey lighten-3">
         <div class="py-12"></div>
