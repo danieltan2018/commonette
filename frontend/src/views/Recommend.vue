1f2ad9a7b8944a9885c7a39d37073b9f4985d913
danieltan2018 <50669678+danieltan2018@users.noreply.github.com>
2020-10-14 23:48:51 +0800

    Add YouTube recommendations

--- frontend/src/views/Recommend.vue
+++ frontend/src/views/Recommend.vue
@@ -1,56 +1,54 @@
 <template>
-  <v-card class="mx-auto">
-
-    <v-container fluid>
-      <v-row dense>
-        <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
-          <v-card>
-            <v-img :src="card.src" class="white--text align-end" height="500px">
-              <v-card-title v-text="card.title"></v-card-title>
-            </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
+  <v-sheet class="mx-auto" elevation="8">
+    <span>
+      <h2>YouTube Recommendations</h2>
+    </span>
+    <v-slide-group class="pa-4" active-class="success" show-arrows>
+      <v-slide-item v-for="video in youtubeResults" :key="video.id">
+        <v-container grid-list-md>
+          <v-card class="mx-auto" max-width="300px">
+            <v-img :src="video.snippet.thumbnails.medium.url" contain></v-img>
+            <v-card-title v-text="video.snippet.title" class="h3"></v-card-title>
+            <v-card-subtitle>
+              {{video.snippet.channelTitle}}
+            </v-card-subtitle>
           </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
+        </v-container>
+      </v-slide-item>
+    </v-slide-group>
+  </v-sheet>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data: () => ({
-    cards: [
-      {
-        title: "Pre-fab homes",
-        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
-        flex: 4,
-      },
-      {
-        title: "Favorite road trips",
-        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
-        flex: 4,
-      },
-      {
-        title: "Best airlines",
-        src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
-        flex: 4,
-      },
-    ],
+    youtubeParams: {
+      category: 0,
+    },
+    youtubeResults: {},
   }),
+  created() {
+    this.initialise();
+  },
+  methods: {
+    initialise() {
+      // Insert api call to backend
+      // Update dummy values
+      this.youtubeParams.category = 28;
+      this.renderYoutube();
+    },
+    renderYoutube() {
+      axios
+        .get(
+          "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&regionCode=SG&videoCategoryId=" +
+            this.youtubeParams.category +
+            "&key=AIzaSyA7Y61l8cbCs3iBaovaUT9iv8eczTikK9k"
+        )
+        .then((response) => {
+          this.youtubeResults = response.data.items;
+        });
+    },
+  },
 };
 </script>
