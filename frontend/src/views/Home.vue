bda37d59eaa72f9df8e54043920527cd2f7960fc
suicidal-muffin <62653896+suicidal-muffin@users.noreply.github.com>
2020-10-15 19:36:11 +0800

    Added cards (list of API used)
    i also formatted everything for better readability. Not sure if the change is reflected here.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,54 +3,112 @@
     <v-main>
       <v-dialog v-model="createRoomPopup" max-width="350">
         <v-card>
-          <v-card-title class="headline justify-center">Create New Room</v-card-title>
+          <v-card-title class="headline justify-center"
+            >Create New Room</v-card-title
+          >
           <v-card-text>
             <v-form @submit.prevent v-model="valid" ref="form">
-              <v-text-field label="Enter a room name" v-model="roomName" :rules="inputRequiredRule" autofocus></v-text-field>
+              <v-text-field
+                label="Enter a room name"
+                v-model="roomName"
+                :rules="inputRequiredRule"
+                autofocus
+              ></v-text-field>
               <p v-if="errors" class="red--text">
                 {{ errorMessage }}
               </p>
-              <v-btn color="primary" :loading="loading1" @click="loader = 'loading1'" :disabled="!valid" v-on:click="createRoom">Enter</v-btn>
+              <v-btn
+                color="primary"
+                :loading="loading1"
+                @click="loader = 'loading1'"
+                :disabled="!valid"
+                v-on:click="createRoom"
+                >Enter</v-btn
+              >
             </v-form>
           </v-card-text>
         </v-card>
       </v-dialog>
       <v-dialog v-model="joinRoomPopup" max-width="350">
         <v-card>
-          <v-card-title class="headline justify-center">Join Existing Room</v-card-title>
+          <v-card-title class="headline justify-center"
+            >Join Existing Room</v-card-title
+          >
           <v-card-text>
             <v-form @submit.prevent v-model="valid" ref="form">
-              <v-text-field label="Enter the room code" v-model="roomCode" :rules="inputRequiredRule" autofocus></v-text-field>
+              <v-text-field
+                label="Enter the room code"
+                v-model="roomCode"
+                :rules="inputRequiredRule"
+                autofocus
+              ></v-text-field>
               <p v-if="errors" class="red--text">
                 {{ errorMessage }}
               </p>
-              <v-btn color="primary" :loading="loading2" @click="loader = 'loading2'" :disabled="!valid" v-on:click="joinRoom">Enter</v-btn>
+              <v-btn
+                color="primary"
+                :loading="loading2"
+                @click="loader = 'loading2'"
+                :disabled="!valid"
+                v-on:click="joinRoom"
+                >Enter</v-btn
+              >
             </v-form>
           </v-card-text>
         </v-card>
       </v-dialog>
 
-      <v-img src="https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80">
+      <v-img
+        src="https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80"
+      >
         <v-theme-provider dark>
           <v-container fill-height>
             <v-row align="center" class="white--text mx-auto" justify="center">
               <v-col class="white--text text-center" cols="12" tag="h1">
-                <span :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']" class="font-weight-light">
+                <span
+                  :class="[
+                    $vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2',
+                  ]"
+                  class="font-weight-light"
+                >
                   WELCOME TO
                 </span>
-                <br>
-                <span :class="[$vuetify.breakpoint.smAndDown ? 'display-3': 'display-4']" class="font-weight-black">
+                <br />
+                <span
+                  :class="[
+                    $vuetify.breakpoint.smAndDown ? 'display-3' : 'display-4',
+                  ]"
+                  class="font-weight-black"
+                >
                   COMMONETTE
                 </span>
               </v-col>
               <v-row justify="center">
-                <v-btn elevation="2" large class="ma-2" color="primary" v-on:click="createRoomPopup = true">
+                <v-btn
+                  elevation="2"
+                  large
+                  class="ma-2"
+                  color="primary"
+                  v-on:click="createRoomPopup = true"
+                >
                   Create Room
                 </v-btn>
-                <v-btn elevation="2" large class="ma-2" color="secondary" @click="$vuetify.goTo('#about')">
+                <v-btn
+                  elevation="2"
+                  large
+                  class="ma-2"
+                  color="secondary"
+                  @click="$vuetify.goTo('#about')"
+                >
                   About
                 </v-btn>
-                <v-btn elevation="2" large class="ma-2" color="primary" v-on:click="joinRoomPopup = true">
+                <v-btn
+                  elevation="2"
+                  large
+                  class="ma-2"
+                  color="primary"
+                  v-on:click="joinRoomPopup = true"
+                >
                   Join Room
                 </v-btn>
               </v-row>
@@ -67,10 +125,18 @@
             <v-divider class="mb-1"></v-divider>
             <v-divider></v-divider>
           </v-responsive>
-          <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720">
+          <v-responsive
+            class="mx-auto title font-weight-light mb-8"
+            max-width="720"
+          >
             info about commonette here
           </v-responsive>
-          <v-btn color="grey" v-on:click="navigateRoute('/recommend')" outlined large>
+          <v-btn
+            color="grey"
+            v-on:click="navigateRoute('/recommend')"
+            outlined
+            large
+          >
             <span class="grey--text text--darken-1 font-weight-bold">
               Recommendation Page (temporary)
             </span>
@@ -88,7 +154,12 @@
             <v-divider></v-divider>
           </v-responsive>
           <v-row>
-            <v-col v-for="({ icon, title, text }, i) in mediums" :key="i" cols="12" md="3">
+            <v-col
+              v-for="({ icon, title, text }, i) in mediums"
+              :key="i"
+              cols="12"
+              md="3"
+            >
               <v-card class="py-12 px-4" color="grey lighten-5" flat>
                 <v-theme-provider dark>
                   <div>
@@ -97,9 +168,11 @@
                     </v-avatar>
                   </div>
                 </v-theme-provider>
-                <v-card-title class="justify-center font-weight-black text-uppercase" v-text="title"></v-card-title>
-                <v-card-text class="subtitle-1" v-text="text">
-                </v-card-text>
+                <v-card-title
+                  class="justify-center font-weight-black text-uppercase"
+                  v-text="title"
+                ></v-card-title>
+                <v-card-text class="subtitle-1" v-text="text"> </v-card-text>
               </v-card>
             </v-col>
           </v-row>
@@ -108,23 +181,96 @@
       </section>
 
       <v-theme-provider dark>
-        <v-parallax :height="$vuetify.breakpoint.smAndDown ? 700 : 200" src="https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80">
-          <v-container fill-height>
-            <h2 class="mx-auto black--text display-2 font-weight-bold mb-3" max-width="720">
-              info about commonette here
+        <v-parallax
+          :height="$vuetify.breakpoint.smAndDown ? 700 : 1000"
+          src="https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80"
+        >
+          <v-container>
+            <div class="py-12"></div>
+            <h2 class="display-2 font-weight-bold mb-3">APIs Used</h2>
+            <v-responsive class="mx-auto mb-12" width="56">
+              <v-divider class="mb-1"></v-divider>
+              <v-divider></v-divider>
+            </v-responsive>
+            <v-row dense>
+              <v-col v-for="(item, i) in items" :key="i" cols="4">
+                <v-card :color="item.color" dark>
+                  <div class="d-flex flex-no-wrap justify-space-between">
+                    <div>
+                      <v-card-title
+                        class="headline"
+                        v-text="item.title"
+                        :style="item.textcolor"
+                      ></v-card-title>
+
+                      <v-card-subtitle
+                        v-text="item.description"
+                        :style="item.textcolor"
+                      ></v-card-subtitle>
+
+                      <v-card-actions>
+                        <!-- custom button here -->
+                        <v-btn
+                          v-if="item.artist === 'Ellie Goulding'"
+                          class="ml-2 mt-3"
+                          fab
+                          icon
+                          height="40px"
+                          right
+                          width="40px"
+                        >
+                          <v-icon>mdi-play</v-icon>
+                        </v-btn>
+
+                        <v-btn
+                          v-else
+                          class="ml-2 mt-5"
+                          color="secondary"
+                          rounded
+                          small
+                        >
+                          ABOUT API
+                        </v-btn>
+                      </v-card-actions>
+                    </div>
+
+                    <v-avatar class="ma-3" size="125" tile>
+                      <v-img :src="item.src" contain></v-img>
+                    </v-avatar>
+                  </div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+          <v-responsive class="mx-auto mb-12" width="56">
+            <v-divider class="mb-1"></v-divider>
+            <v-divider></v-divider>
+          </v-responsive>
+          <v-container>
+            <h2 class="display-1 font-weight-bold mb-3 black--text">
+              What are you waiting for?
             </h2>
             <v-row class="mx-auto" justify="center">
-              <v-btn elevation="2" large class="ma-2" v-on:click="createRoomPopup = true">
+              <v-btn
+                elevation="2"
+                large
+                class="ma-2"
+                v-on:click="createRoomPopup = true"
+              >
                 Create Room
               </v-btn>
-              <v-btn elevation="2" large v-on:click="joinRoomPopup = true" class="ma-2">
+              <v-btn
+                elevation="2"
+                large
+                v-on:click="joinRoomPopup = true"
+                class="ma-2"
+              >
                 Join Room
               </v-btn>
             </v-row>
           </v-container>
         </v-parallax>
       </v-theme-provider>
-
     </v-main>
   </div>
 </template>
@@ -147,6 +293,70 @@ export default {
       loading1: false,
       loading2: false,
       loading3: false,
+
+      items: [
+        {
+          color: "#fafafa",
+          textcolor: "color: black",
+          src:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/1280px-YouTube_full-color_icon_%282017%29.svg.png",
+          title: "YouTube Data API",
+          description: "YouTube info here",
+          site: "https://developers.google.com/youtube/v3/docs/videos/list",
+        },
+        {
+          color: "#191414",
+          textcolor: "color: #FFFFFF",
+          src:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png",
+          title: "Spotify Search API",
+          description:
+            "Get Spotify Catalog information about albums, artists, playlists, tracks, shows or episodes that match a keyword string.",
+          site:
+            "https://developer.spotify.com/documentation/web-api/reference/search/search/",
+        },
+        {
+          color: "#191414",
+          textcolor: "color: #FFFFFF",
+          src:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png",
+          title: "Spotify Recommendations API",
+          description:
+            "Create a playlist-style listening experience based on seed artists, tracks and genres.",
+          site:
+            "https://developer.spotify.com/documentation/web-api/reference/browse/get-recommendations/",
+        },
+        {
+          color: "#fafafa",
+          textcolor: "color: black",
+          src:
+            "https://upload.wikimedia.org/wikipedia/commons/b/ba/Google_Books_logo_2015.svg",
+          title: "Google Books API",
+          description: "Info about google books here",
+          site: "",
+        },
+        {
+          color: "#fafafa",
+          textcolor: "color: black",
+          src:
+            "https://s3.amazonaws.com/mashape-production-logos/apis/5c35064ce4b0ddd96d1a3a36_medium",
+          title: "Entertainment Data Hub API",
+          description: "Info about Data Hub API here",
+          site:
+            "https://rapidapi.com/IVALLC/api/entertainment-data-hub/details",
+        },
+        {
+          color: "#fafafa",
+          textcolor: "color: black",
+          src:
+            "https://rapidapi-prod-apis.s3.amazonaws.com/64ed0ed2-6103-4f9b-ab95-f8cc98c7e40c.png",
+          title: "OTT details API",
+          description:
+            "Get Streaming details of Movie and TV Shows. We support 150+ Streaming platforms in US and India such as HBO, YouTube, Netflix , Primve Video, Hotstar, Hulu, etc . ",
+          site:
+            "https://rapidapi.com/gox-ai-gox-ai-default/api/ott-details/endpoints",
+        },
+      ],
 
       mediums: [
         {
@@ -173,17 +383,17 @@ export default {
       ],
     };
   },
-  
+
   watch: {
-      loader () {
-        const l = this.loader
-        this[l] = !this[l]
+    loader() {
+      const l = this.loader;
+      this[l] = !this[l];
 
-        setTimeout(() => (this[l] = false), 1500)
+      setTimeout(() => (this[l] = false), 1500);
 
-        this.loader = null
-      },
+      this.loader = null;
     },
+  },
 
   methods: {
     navigateRoute(newpath) {
