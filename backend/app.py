dd14c595947ed9ba3e383a51c4980f6284b55e42
syafiqahmr <syafiqahmr.2018@sis.smu.edu.sg>
2020-10-16 00:24:56 +0800

    Complete backend recommend

--- backend/app.py
+++ backend/app.py
@@ -74,6 +74,9 @@ def update(room_code):
         room = room_ref.document(room_code).get()
         room = room.to_dict()
 
+        if (len(room["questionnaire"]) == 0):
+            return "At least one user needs to complete the questionnaire", 400
+
         return jsonify(utility.generate_api(room["questionnaire"])), 200
     except Exception as e:
         return f"{e}", 400
