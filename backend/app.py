55fa56973db9843388808665065f5e04f8cdff1f
syafiqahmr <syafiqahmr.2018@sis.smu.edu.sg>
2020-10-15 18:09:20 +0800

    Modify backend db to firestore

--- backend/app.py
+++ backend/app.py
@@ -1,71 +1,55 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
+from firebase_admin import credentials, firestore, initialize_app
+
 import json
 import utility
 
+
 app = Flask(__name__)
-dbURL = 'mysql+mysqlconnector://admin:Commonette234!@commonette.ciygr11ayijs.ap-southeast-1.rds.amazonaws.com:3306/commonette'
-app.config['SQLALCHEMY_DATABASE_URI'] = dbURL
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-CORS(app)
-
-
-class Room(db.Model):
-    __tablename__ = 'room'
-    code = db.Column(db.String(6), primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-
-    def json(self):
-        dto = {
-            'code': self.code,
-            'name': self.name
-        }
-        return dto
+# Initialize Firestore DB
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+room_ref = db.collection('rooms')
 
 
-engine = create_engine(dbURL)
-if not database_exists(engine.url):
-    create_database(engine.url)
-db.create_all()
-db.session.commit()
 
-
-@app.route("/create-room/<string:room_name>", methods=['GET'])
-def create_room(room_name):
+@app.route('/create-room/<string:room_name>', methods=['GET'])
+def create(room_name):
     random_code = utility.get_random_string(6)
 
     # checks if random_code already exists
-    while Room.query.filter_by(code=random_code).first():
+    room = room_ref.document(random_code).get()
+    while (room.to_dict() != None):
+        print ("here")
         random_code = utility.get_random_string(6)
+        room = room_ref.document(random_code).get()
 
-    room = Room(code=random_code, name=room_name)
 
     try:
-        db.session.add(room)
-        db.session.commit()
-    except:
-        return jsonify({"message": "An error occurred creating the room."}), 500
+        obj = {"room_name": room_name, "questionnaire":[]}
+        room_ref.document(random_code).set(obj)
+        return jsonify({"room_code": random_code, "room_name": room_name}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
-    return jsonify(room.json()), 200
-
-# to return api stuff afterwards
-
-
-@app.route("/room/<string:room_code>", methods=['GET'])
+@app.route('/room/<string:room_code>', methods=['GET'])
 def get_room(room_code):
-    room = Room.query.filter_by(code=room_code).first()
-
-    if room:
-        return jsonify(room.json()), 200
-    else:
-        return jsonify({'message': 'Invalid room code'}), 404
-
+    """
+        read() : Fetches documents from Firestore collection as JSON.
+        todo : Return document that matches query ID.
+        all_todos : Return all documents.
+    """
+    try:
+        room = room_ref.document(room_code).get()
+        if room.to_dict() == None: #room code doesn't exist
+            return jsonify({'message': 'Invalid room code'}), 404
+        return jsonify(room.to_dict()), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
 # This is for flask app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
