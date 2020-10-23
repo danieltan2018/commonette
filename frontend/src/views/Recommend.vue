da654b7ba740780a9ccd0ba90c1466278ead289b
Daniel Tan <50669678+danieltan2018@users.noreply.github.com>
2020-10-24 02:54:26 +0800

    Add shuffle function

--- frontend/src/views/Recommend.vue
+++ frontend/src/views/Recommend.vue
@@ -307,6 +307,19 @@ export default {
     closeDetails() {
       this.showDetails = !this.showDetails;
     },
+    shuffle(arr) {
+      var ctr = arr.length,
+        temp,
+        index;
+      while (ctr > 0) {
+        index = Math.floor(Math.random() * ctr);
+        ctr--;
+        temp = arr[ctr];
+        arr[ctr] = arr[index];
+        arr[index] = temp;
+      }
+      return arr;
+    },
   },
 };
 </script>
