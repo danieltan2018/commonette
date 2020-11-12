86af8822697f4ef819c0bbbded4a272802fd78bd
syafiqahmr <syafiqahmr.2018@sis.smu.edu.sg>
2020-11-12 19:33:42 +0800

    Add copy code in dashboard

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,13 +3,15 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
 import Axios from "axios";
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Clipboard from "v-clipboard";
 
 Vue.config.productionTip = false
 Vue.prototype.$http = Axios;
 Vue.prototype.$http.defaults.baseURL = "https://commonapi.smusis.com";
 Vue.prototype.$bus = new Vue()
+Vue.use(Clipboard);
 
 new Vue({
   vuetify,
